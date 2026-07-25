febb4a2517e46fc6139821b07dde2374d8057691
EchoQuill <safnaj059@gmail.com>
2026-07-25 09:17:27 +0530

    chore: perhaps we dont need that?

--- src/routes/(pages)/+layout.ts
+++
@@ -1,4 +0,0 @@
-// For github pages to work correctly (hopefully)
-// I am not good with these haha, no idea what I am doing, just hoping that this works.. :>
-export const prerender = true;
-export const ssr = false;
