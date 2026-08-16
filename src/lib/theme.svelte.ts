97e9604adfff7bda845c029639e225af8e721866
EchoQuill <safnaj059@gmail.com>
2026-08-16 20:10:22 +0530

    fix: incorrect theme loading + add dark theme

--- src/lib/theme.svelte.ts
+++ src/lib/theme.svelte.ts
@@ -1,25 +1,28 @@
 // This file loads theme of user's preference from cache.
 
 export class Theme {
-    current_theme = $state("caramellatte");
-    allotted_themes = ["caramellatte", "coffee", "strawberry"];
+    current_theme = $state("black");
+    allotted_themes = ["caramellatte", "coffee", "strawberry", "black"];
 
     public initTheme() {
         // Called during mount at layout file.
         if (typeof window === "undefined") return;
 
-        const savedTheme = localStorage.getItem("theme");
+        let savedTheme: any = localStorage.getItem("theme");
 
-        if (savedTheme && this.allotted_themes.includes(savedTheme)) {
-            // Set theme from local store.
-            document.documentElement.dataset.theme = savedTheme;
+        if (!savedTheme && !this.allotted_themes.includes(savedTheme)) {
+            savedTheme = "black";
         }
+        // Set theme from local store.
+        document.documentElement.dataset.theme = savedTheme;
+        this.current_theme = savedTheme;
     }
 
     public setTheme(theme: string) {
         if (this.allotted_themes.includes(theme)) {
             localStorage.setItem("theme", theme);
             document.documentElement.dataset.theme = theme;
+            this.current_theme = theme;
         } else {
             console.error("Failed to set theme - Not in allotted_themes.")
         }
